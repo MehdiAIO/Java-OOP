f3f9a5fed8c875c2f218eaba4c1faf81f2409418
mehdiaio <elmahdi.aitouzdi@gmail.com>
2025-01-21 16:19:44 +0100

    abstraction

---
+++ Car.java
@@ -0,0 +1,6 @@
+public class Car extends Vehicule {
+    @Override
+    void go() {
+        System.out.println("the driver is driving the car");
+    }
+}
--- Script.java
+++ Script.java
@@ -1,12 +1,10 @@
 public class Script {
+    // abstract = abstract classes cannot be instantiated, but they can have a
+    // subclass
+
+    // abstract methods are declared without an implementation
     public static void main(String[] args) {
-
-        Person p1 = new Person("mehdi", 21);
-
-        Hero h1 = new Hero("batman", 39, "Money$$$");
-
-        String result = h1.toString();
-        System.out.println(result);
-
+        Car c1 = new Car();
+        c1.go();
     }
 }
