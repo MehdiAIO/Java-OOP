148a983b777210ef0789f281417c57278e8ce696
mehdiaio <elmahdi.aitouzdi@gmail.com>
2025-05-28 16:12:55 +0100

    Composition

--- Main.java
+++ Main.java
@@ -1,16 +1,15 @@
 public class Main {
-    // Aggregation = Represents a "has-a" relationship between objects.
-    //               One Object contains another objects as part of its structure,
-    //               but the contained object/s can exist independently.
     public static void main(String[] args) {
-        Book book1 = new Book("Surrounded by idiots", 170);
-        Book book2 = new Book("the law of power", 300);
-        Book book3 = new Book("this is marketing", 237);
+        // Composition = Represents a "part-of" relationship between objects.
+        //               For example, an Engine is "part of" a Car.
+        //               Allows complex objects to be constructed from smaller objects.
 
-        Book[] books = {book1,book2,book3};
+        Car car = new Car("Corvette", 2025, "V8");
 
-        Library library = new Library("Rabat National Library", 1917,books);
+        System.out.println(car.model);
+        System.out.println(car.year);
+        System.out.println(car.engine.type);
 
-        library.getDetails();
+        car.start();
     }
 }
