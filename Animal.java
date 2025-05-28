59e0f60c68f04a5c4352163f86eeb0390556ebc5
mehdiaio <elmahdi.aitouzdi@gmail.com>
2025-05-28 13:42:33 +0100

    getters & setters

--- Animal.java
+++
@@ -1,5 +0,0 @@
-public class Animal {
-    public void speak() {
-        System.out.println("the animal goes nothing");
-    }
-}
--- Cat.java
+++
@@ -1,6 +0,0 @@
-public class Cat extends Animal {
-    @Override
-    public void speak() {
-        System.out.println("the animal goes \"meow\"");
-    }
-}
