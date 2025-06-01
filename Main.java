f7427d194ef9d401be565c21a1db2fa74f20b78f
mehdiaio <elmahdi.aitouzdi@gmail.com>
2025-06-01 14:57:51 +0100

    ArrayList

--- Main.java
+++ Main.java
@@ -1,35 +1,23 @@
-public class Main{
-    public static void main(String[] args){
-        // Wrapper classes = Allow primitive values (int, char, double, boolean)
-        //                                   to be used as objects. "Wrap them in an object"
-        //                                   Generally, don't wrap primitives unless you need an object.
-        //                                   Allows use of Collections Framework and static utility methods.
+import java.util.ArrayList;
 
-        // Autoboxing
-        // Integer a = 123;
-        // Double b = 3.14;
-        // Character c = '$';
-        // Boolean d = true;
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> fruits = new ArrayList<>();
 
-        // Unboxing
-        // int x = a;
-        // double y = b;
-        // char w = c;
-        // boolean z = d;
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Orange");
 
-        // String a = Integer.toString(123);
-        // String b = Double.toString(3.13);
-        // String c = Character.toString('@');
-        // String d = Boolean.toString(true);
+        System.out.println(fruits); // [Apple, Banana, Orange]
 
-        // String x = a + b + c + d;
-        // System.out.println(x);
+        fruits.remove("Banana");
+        System.out.println(fruits.get(1)); // Orange
 
-        // int a = Integer.parseInt("123");
-        // double b = Double.parseDouble("3.14");
-        // char c = "Pizza".charAt(0);
-        // boolean d = Boolean.parseBoolean("true");
+        fruits.set(0, "Mango");
+        System.out.println(fruits); // [Mango, Orange]
 
-        
+        System.out.println(fruits.contains("Apple")); // false
+        System.out.println(fruits.size()); // 2
+        System.out.println(fruits.isEmpty());
     }
 }
